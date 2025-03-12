608e879bab7e7b6b8f6ed061136bb570ddc316ba
kasia mirowska <kasiamirowska@kasias-MacBook-Pro-2.local>
2025-03-12 14:40:05 -0400

    resume fix?

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -21,8 +21,8 @@ export default class Nav extends React.Component {
       <div className="nav">
         <div className="links">
           <a
-            // href="/files/KASIA_MIROWSKA_RESUME.pdf"
-            href="https://kasiamirowska.netlify.app/files/KASIA_MIROWSKA_RESUME.pdf"
+            href="/files/KASIA_MIROWSKA_RESUME.pdf"
+            //href="https://kasiamirowska.netlify.app/files/KASIA_MIROWSKA_RESUME.pdf"
             target="_blank"
             rel="noopener noreferrer"
             activeclass="link-active"
